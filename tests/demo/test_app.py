d8bfdff8be91f47039b75a4ca939a346c0c670bb
Kevin Shindel <kevin.shindel@yahoo.com>
2025-11-24 20:26:17 +0100

    update CI pipeline

--- tests/demo/test_app.py
+++ tests/demo/test_app.py
@@ -1,22 +1,24 @@
-import unittest
 from src.demo.app import say_hello
+import pytest
 
 
-class TestSayHello(unittest.TestCase):
+def test_say_hello():
+    with pytest.raises(AssertionError):
+        assert say_hello('Bob') == 'Hello Stefan!'
 
-    def test_say_hello(self):
-        # this assertion is *supposed* to fail, so we wrap it with assertRaises
-        with self.assertRaises(AssertionError):
-            self.assertEqual(say_hello("Stefan"), "Hello Bob!")
 
-    def test_say_hello_multi(self):
-        assert say_hello('Stefan') == 'Hello Stefan!'
+def test_say_hello_single():
+    assert say_hello('Stefan') == 'Hello Stefan!'
 
-        bob, empty, none, integer, li, tup = names
 
-        assert say_hello(bob) == 'Hello Bob!'
-        assert say_hello(empty) == 'Hello !'
-        assert say_hello(none) == 'Hello None!'
-        assert say_hello(integer) == 'Hello 123!'
-        assert say_hello(li) == 'Hello []!'
-        assert say_hello(tup) == 'Hello ()!'
+def test_say_hello_multi(names):
+    assert say_hello('Stefan') == 'Hello Stefan!'
+
+    bob, empty, none, integer, li, tup = names
+
+    assert say_hello(bob) == 'Hello Bob!'
+    assert say_hello(empty) == 'Hello !'
+    assert say_hello(none) == 'Hello None!'
+    assert say_hello(integer) == 'Hello 123!'
+    assert say_hello(li) == 'Hello []!'
+    assert say_hello(tup) == 'Hello ()!'
